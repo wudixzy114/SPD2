0e83fb5eec37c38ce8576b08c7ae61c27b9c6604
xiezongyu <xie-zy23@mails.tsinghua.edu.cn>
2025-10-30 22:08:40 +0800

    实现GameManager事件监听和玩家输入处理

--- assets/scripts/core/GameManager.ts
+++ assets/scripts/core/GameManager.ts
@@ -1,14 +1,52 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, director, game, Vec2 } from "cc";
+import { EventManager } from "./EventManager";
+import { GameEvent } from "../common/GameEvent";
 const { ccclass, property } = _decorator;
 
-@ccclass('GameManager')
+@ccclass("GameManager")
 export class GameManager extends Component {
-    start() {
+  protected onLoad(): void {
+    director.addPersistRootNode(this.node);
 
-    }
+    EventManager.instance.on(
+      GameEvent.INPUT_MOVE_INTENT,
+      this.onPlayerMoveIntent,
+      this
+    );
+    EventManager.instance.on(
+      GameEvent.INPUT_WAIT_INTENT,
+      this.onPlayerWaitIntent,
+      this
+    );
+  }
 
-    update(deltaTime: number) {
-        
-    }
+  protected onDestroy(): void {
+    EventManager.instance.off(
+      GameEvent.INPUT_MOVE_INTENT,
+      this.onPlayerMoveIntent,
+      this
+    );
+    EventManager.instance.off(
+      GameEvent.INPUT_WAIT_INTENT,
+      this.onPlayerWaitIntent,
+      this
+    );
+  }
+
+  protected start(): void {
+    console.log("Game architecture initialized. Waiting for input...");
+    EventManager.instance.emit(GameEvent.GAME_START);
+  }
+
+  private onPlayerMoveIntent(direction: Vec2) {
+    console.log(
+      `[GameManager] Received move intent: (${direction.x}, ${direction.y})`
+    );
+    // Future logic for player action will be here
+  }
+
+  private onPlayerWaitIntent() {
+    console.log(`[GameManager] Received wait intent.`);
+    // Future logic for player wait action will be here
+  }
 }
-
